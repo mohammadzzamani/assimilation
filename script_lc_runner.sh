e99e20432ecd2fc2199546a706d71e56f2d6c10a
mzamani <mzamani@cs.stonybrook.edu>
2018-12-04 10:29:16 -0500

    write s.th. to hadoop

---
+++ script_lc_runner.sh
@@ -0,0 +1,47 @@
+#!/bin/ksh
+set -v
+
+#export PYSPARK_PYTHON=/opt/app/anaconda2/python27/bin/python;
+export SPARK_HOME=/opt/mapr/spark/spark-2.0.1/
+export PYSPARK_PYTHON=python2.7
+
+#hadoop fs -get $2/files/* .
+
+~/../../opt/mapr/spark/spark-2.0.1/bin/spark-submit\
+--master yarn \
+--deploy-mode client \
+--executor-memory 4g \
+--driver-memory 5g \
+--jars ~/hadoopPERMA/jars/hadoop-lzo-0.4.21-SNAPSHOT.jar \
+spark_lang_change_runner.py
+
+#--driver-library-path /usr/hdp/current/hadoop-client/lib/native/Linux-amd64-64 \
+#--num-executors 40 \
+#--executor-cores 2 \
+#--master yarn \
+#--deploy-mode cluster \
+#--executor-memory 10g \
+#--driver-memory 15g \
+#--conf spark.yarn.executor.memoryOverhead=1024 \
+#--conf spark.sql.shuffle.partitions=1000 \
+#--conf spark.io.compression.codec=lzf \
+#--conf spark.network.timeout=800 \
+#--conf spark.rpc.askTimeout=800 \
+#--conf spark.locality.wait=10s \
+#--conf spark.task.maxFailures=10000 \
+#--conf spark.shuffle.manager=SORT \
+#--conf spark.akka.frameSize=300 \
+#--conf spark.yarn.max.executor.failures=1000 \
+#--conf spark.shuffle.consolidateFiles=true \
+#--conf spark.shuffle.service.enabled=true \
+#--conf spark.default.parallelism=2000 \
+#--conf spark.driver.maxResultSize=2048 \
+#--py-files conf.zip,\
+#spark_schema.zip,shared.zip,readers.zip,\
+#goozie.zip,\
+#descriptors.zip \
+#--files /usr/hdp/current/spark-client/conf/hive-site.xml \
+#--jars tez-api.jar,\
+#datanucleus-rdbms.jar,datanucleus-core.jar,\
+#datanucleus-api-jdo.jar,commons-csv-1.1.jar,spark-csv_2.10-1.5.0.jar \
+#prod_rg_inventory.py run prod_rg_inventory rg_inventory $1
---
+++ spark_lang_change_runner.py
@@ -0,0 +1,111 @@
+from pyspark.sql.types import StructType, StructField, StringType
+from pyspark import SparkConf, SparkContext, SQLContext
+from pyspark.sql import DataFrame
+import constants as c
+import os
+
+
+def build_spark_context_cluster(app_name):
+    global sc
+    conf = SparkConf()
+    conf.setAppName(app_name)
+    sc = SparkContext(conf=conf)
+    sc.setLogLevel("ERROR")
+    sc = SQLContext(sc)
+    return sc
+
+
+# def read_data_frame(file_name, schema, has_header='false', delimiter='\001', compress=None):
+def read_data_frame(file_name, schema=None, has_header='false', delimiter=',', compress=None):
+    """
+    :param schema:
+    :type schema: pyspark.sql.types.StructType
+    :param file_name: full HDFS path of CSV file name
+    :param has_header: only applicable when schema is passed
+    :type has_header: str ('false' or 'true')
+    :type file_name: str
+    :return:
+    :rtype: DataFrame
+    """
+    global sc
+    if schema is None:
+        if compress:
+            assert compress in [c.GZIP, c.SNAPPY]
+            df = sc.read \
+                .format('com.databricks.spark.csv') \
+                .options(header='true', inferschema='true', delimiter=delimiter, codec=compress) \
+                .load(file_name)
+        else:
+            df = sc.read \
+                .format('com.databricks.spark.csv') \
+                .options(header='true', inferschema='true', delimiter=delimiter) \
+                .load(file_name)
+    else:
+        if compress:
+            assert compress in [c.GZIP, c.SNAPPY]
+            df = sc.read \
+                .format('com.databricks.spark.csv') \
+                .options(header=has_header, inferschema='false', delimiter=delimiter, codec=compress) \
+                .schema(schema) \
+                .load(file_name)
+        else:
+            df = sc.read \
+                .format('com.databricks.spark.csv') \
+                .options(header=has_header, inferschema='false', delimiter=delimiter) \
+                .schema(schema) \
+                .load(file_name)
+
+        df.printSchema()
+        print ('df.count: ', df.count())
+        print ('df.columns: ', df.columns)
+        df = df.repartition(100)
+    return df
+
+def write_data_frame(df, output_path, delimiter=',', repartition=None, coalesce=None, compress=None):
+    global sc
+    if repartition is not None:
+        df = df.repartition(repartition)
+    if coalesce is not None:
+        df = df.coalesce(coalesce)
+    df.cache()
+
+    if compress:
+        assert compress in [c.GZIP, c.SNAPPY]
+        df.write \
+            .options(header='true', delimiter=delimiter) \
+            .option("codec", compress) \
+            .format('com.databricks.spark.csv') \
+            .mode("overwrite") \
+            .save(output_path)
+    else:
+        df.write \
+            .options(header='true', delimiter=delimiter) \
+            .format('com.databricks.spark.csv') \
+            .mode("overwrite") \
+            .save(output_path)
+
+
+def run_command(cmd):
+    """Return (status, output) of executing cmd in a shell."""
+    pipe = os.popen('{ ' + cmd + '; } 2>&1', 'r')
+    # text = pipe.read()
+    # sts = pipe.close()
+    # if sts is None: sts = 0
+    # if text[-1:] == '\n': text = text[:-1]
+    # return sts, text
+
+
+if __name__ == "__main__":
+    global sc
+    sc = build_spark_context_cluster( 'running my bundle' )
+    ROOT_DIR = '/user/mzamani/'
+    df = read_data_frame(file_name=ROOT_DIR + 'feat.1gram.msg_1001_nrt_small.userid_week', schema = StructType([StructField(field, StringType(), True) for field in ['group_id', 'feat', 'value', 'group_norm']]), compress = 'gzip' )
+    df.cache()
+    print ('df: ')
+    df.show()
+
+    output_dir = ROOT_DIR + 'test_dir/'
+    full_output_path = output_dir + 'test1'
+    run_command('hadoop fs -mkdir -p %s' % output_dir)
+    write_data_frame(df, full_output_path)
+
