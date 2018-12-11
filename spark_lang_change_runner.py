85846bbab53dfa150955b6e6b8148581b8bc9576
mzamani <mzamani@cs.stonybrook.edu>
2018-12-10 21:24:21 -0500

    write to hadoop using predefined schema

--- spark_lang_change_runner.py
+++ spark_lang_change_runner.py
@@ -7,10 +7,11 @@ import os
 
 GZIP = 'gzip'
 SNAPPY = 'snappy'
-topic_table = 'feat.2000fb.msg_1001_nrt.userid_week'
-ngram_table = 'feat.1gram.msg_1001_nrt_small.userid_week'
-accounts_table = 'twitter_accounts_1001_uniques.csv'
-network_table = 'twitter_network_1001.csv'
+ROOT_DIR = '/user/mzamani/'
+topics_table = 'feat.2000fb.msg_1001_nrt.userid_week'
+ngrams_table = 'feat.1gram.msg_1001_nrt_small.userid_week'
+accounts_table = 'twitter_accounts_1001_uniques'
+network_table = 'twitter_network_1001'
 
 def build_spark_context_cluster(app_name):
     global sc
@@ -61,10 +62,10 @@ def read_data_frame(file_name, schema=None, has_header='false', delimiter=',', c
                 .schema(schema) \
                 .load(file_name)
 
-        df.printSchema()
-        print ('df.count: ', df.count())
-        print ('df.columns: ', df.columns)
-        df = df.repartition(100)
+    df.printSchema()
+    print ('df.count: ', df.count())
+    print ('df.columns: ', df.columns)
+    df = df.repartition(100)
     return df
 
 def write_data_frame(df, output_path, delimiter=',', repartition=None, coalesce=None, compress=None):
@@ -74,6 +75,9 @@ def write_data_frame(df, output_path, delimiter=',', repartition=None, coalesce=
     if coalesce is not None:
         df = df.coalesce(coalesce)
     df.cache()
+
+    print ('df.schema: ')
+    df.printSchema()
 
     if compress:
         assert compress in [GZIP, SNAPPY]
@@ -105,30 +109,42 @@ def run_command(cmd):
 if __name__ == "__main__":
     global sc
     sc = build_spark_context_cluster( 'running my bundle' )
-    ROOT_DIR = '/user/mzamani/'
 
 
     #### read topics_df, accounts_df, and network_df:
-    topics_df = read_data_frame(file_name=ROOT_DIR + topic_table, schema=StructType([StructField(field, StringType(), True) for field in ['group_id', 'feat', 'value', 'group_norm']]), compress = 'gzip' )
-    topics_df.cache()
     print ('topics_df: ')
-    topics_df.show()
+    topics_schema = StructType([StructField(field, StringType(), True) for field in
+                                ['group_id', 'feat', 'value', 'group_norm']])
+    topics_df = read_data_frame(file_name=ROOT_DIR + topics_table, schema=topics_schema, compress = 'gzip' )
+    topics_df.cache()
+    # topics_df.show()
 
-    accounts_df = read_data_frame(file_name=ROOT_DIR + accounts_table, schema=None, compress = 'gzip' )
-    accounts_df.cache()
+    print ('ngrams_df: ')
+    ngrams_df = read_data_frame(file_name=ROOT_DIR + ngrams_table, schema=topics_schema, compress = 'gzip' )
+    ngrams_df.cache()
+    # topics_df.show()
+
     print ('accounts_df: ')
+    accounts_schema = StructType([StructField(field, StringType(), True) for field in
+                                  ['counter' ,'id' ,'username' ,'last_message_id' ,'followers_count' ,'friends_count'
+                                      ,'created_at' ,'favourites_count' ,'statuses_count' ,'level'
+                                      ,'recorded_friends_count' ,'recorded_statuses_count']])
+    accounts_df = read_data_frame(file_name=ROOT_DIR + accounts_table, schema=accounts_schema, compress = 'gzip' )
+    accounts_df.cache()
     accounts_df.show()
 
-    network_df = read_data_frame(file_name=ROOT_DIR + network_table, schema=None, compress = 'gzip' )
-    network_df.cache()
     print ('network_df: ')
+    network_schema = StructType([StructField(field, StringType(), True) for field in
+                                  ["counter","source_id","dest_id","likes","mentions","retweets","friends_since"] ])
+    network_df = read_data_frame(file_name=ROOT_DIR + network_table, schema=network_schema, compress = 'gzip' )
+    network_df.cache()
     network_df.show()
     ####
 
 
-
+    #### write dataframe to hadoop storage
     output_dir = ROOT_DIR + 'test_dir/'
-    full_output_path = output_dir + 'test1'
+    full_output_path = output_dir + 'test2'
     run_command('hadoop fs -mkdir -p %s' % output_dir)
-    write_data_frame(accounts_df, full_output_path, repartition=100)
+    write_data_frame(accounts_df, full_output_path, repartition=100, compress=None)
 
